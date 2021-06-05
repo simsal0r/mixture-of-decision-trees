1f36b28daecd6766889397e754a826d67470c2a7
Simeon <simeon.brueggenjuergen@gmail.com>
2021-06-05 11:29:47 +0200

    Code cleaning

--- run_modt.py
+++ run_modt.py
@@ -1,33 +1,33 @@
 import pickle
 from modt.modt import MoDT
 
-data_input = pickle.load( open("datasets/breast_cancer_input.np", "rb" ))
-data_target = pickle.load( open ("datasets/breast_cancer_target.np", "rb"))
+data_input = pickle.load(open("datasets/breast_cancer_input.np", "rb"))
+data_target = pickle.load(open("datasets/breast_cancer_target.np", "rb"))
 
 parameters = {
-        "X" : data_input,
-        "y" : data_target,
-        "n_experts" : 3,
-        "iterations" : 1,
-        "max_depth" : 2,
-        "init_learning_rate" : 10,
-        "learning_rate_decay" : 1,
-        "initialize_with" : "random",
-        "initalization_method" : None,
-        "feature_names" : None,
-        "class_names" : None,
-        "use_2_dim_gate_based_on" : "feature_importance",
-        "use_2_dim_clustering" : False,
-        "black_box_algorithm" : None,
+    "X": data_input,
+    "y": data_target,
+    "n_experts": 3,
+    "iterations": 1,
+    "max_depth": 2,
+    "init_learning_rate": 10,
+    "learning_rate_decay": 1,
+    "initialize_with": "random",
+    "initalization_method": None,
+    "feature_names": None,
+    "class_names": None,
+    "use_2_dim_gate_based_on": "feature_importance",
+    "use_2_dim_clustering": False,
+    "black_box_algorithm": None,
     }
 
 parameters_fit = {
-        "optimization_method" : "least_squares_linear_regression",
-        "add_noise" : False,
-        "use_posterior" : False,
-        }
+    "optimization_method": "least_squares_linear_regression",
+    "add_noise": False,
+    "use_posterior": False,
+    }
 
 
 modt = MoDT(**parameters)
-    #print(modt.learn_rate)
+#print(modt.learn_rate)
 modt.fit(**parameters_fit)
--- tests/modt_test.py
+++ tests/modt_test.py
@@ -5,67 +5,76 @@ import sys, os
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
 from modt.modt import MoDT
 
-def fit_modt(
-            X,
-            y,
-            n_experts,
-            iterations,
-            max_depth,
-            init_learning_rate,
-            learning_rate_decay,
-            initialize_with,
-            initalization_method,
-            feature_names,
-            class_names,
-            use_2_dim_gate_based_on,
-            use_2_dim_clustering,
-            black_box_algorithm,
 
-            optimization_method,
-            add_noise,
-            use_posterior,
-        ):
+def fit_modt(X,
+             y,
+             n_experts,
+             iterations,
+             max_depth,
+             init_learning_rate,
+             learning_rate_decay,
+             initialize_with,
+             initalization_method,
+             feature_names,
+             class_names,
+             use_2_dim_gate_based_on,
+             use_2_dim_clustering,
+             black_box_algorithm,
 
-    modt = MoDT(X,y,n_experts=n_experts,iterations=iterations,
-                init_learning_rate=init_learning_rate,learning_rate_decay=learning_rate_decay,max_depth=max_depth,
-                initialize_with=initialize_with,initalization_method=initalization_method,
-                feature_names = feature_names, class_names = class_names,
+             optimization_method,
+             add_noise,
+             use_posterior):
+
+    modt = MoDT(X=X,
+                y=y,
+                n_experts=n_experts,
+                iterations=iterations,
+                init_learning_rate=init_learning_rate,
+                learning_rate_decay=learning_rate_decay,
+                max_depth=max_depth,
+                initialize_with=initialize_with,
+                initalization_method=initalization_method,
+                feature_names=feature_names,
+                class_names=class_names,
                 black_box_algorithm=black_box_algorithm,
-                use_2_dim_gate_based_on=use_2_dim_gate_based_on,use_2_dim_clustering=use_2_dim_clustering)
+                use_2_dim_gate_based_on=use_2_dim_gate_based_on,
+                use_2_dim_clustering=use_2_dim_clustering)
 
-    modt.fit(optimization_method=optimization_method,add_noise=add_noise,
-                use_posterior=use_posterior)
-                
+    modt.fit(optimization_method=optimization_method,
+             add_noise=add_noise,
+             use_posterior=use_posterior)
+
     return modt
+
 
 class TestMoDT(unittest.TestCase):
 
     def test_parameters(self):
-        data_input = pickle.load( open("datasets/iris_input.pd", "rb" ))
-        data_target = pickle.load( open ("datasets/iris_target.pd", "rb"))
+        data_input = pickle.load(open("datasets/iris_input.pd", "rb"))
+        data_target = pickle.load(open("datasets/iris_target.pd", "rb"))
         parameters = {
-            "X" : data_input,
-            "y" : data_target,
-            "n_experts" : 3,
-            "iterations" : 1,
-            "max_depth" : 2,
-            "init_learning_rate" : 10,
-            "learning_rate_decay" : 1,
-            "initialize_with" : "random",
-            "initalization_method" : None,
-            "feature_names" : None,
-            "class_names" : None,
-            "use_2_dim_gate_based_on" : "feature_importance",
-            "use_2_dim_clustering" : False,
-            "black_box_algorithm" : None,
+            "X": data_input,
+            "y": data_target,
+            "n_experts": 3,
+            "iterations": 1,
+            "max_depth": 2,
+            "init_learning_rate": 10,
+            "learning_rate_decay": 1,
+            "initialize_with": "random",
+            "initalization_method": None,
+            "feature_names": None,
+            "class_names": None,
+            "use_2_dim_gate_based_on": "feature_importance",
+            "use_2_dim_clustering": False,
+            "black_box_algorithm": None,
 
-            "optimization_method" : "least_squares_linear_regression",
-            "add_noise" : False,
-            "use_posterior" : False,
+            "optimization_method": "least_squares_linear_regression",
+            "add_noise": False,
+            "use_posterior": False,
         }
 
-        self.assertTrue(fit_modt(**parameters).duration_fit != None)
+        self.assertTrue(fit_modt(**parameters).duration_fit is not None)
+
 
 if __name__ == '__main__':
     unittest.main()
-    
