05bb5af63edb99d5ba9042efc27fed720a7508c3
Ubuntu <azureuser@benchmarker.33irlzldpo1ejcxfasool0wbbf.ax.internal.cloudapp.net>
2021-06-17 20:09:14 +0000

    experiment learning rate intermediate steel

--- benchmarks/optuna_learning_rate.py
+++ benchmarks/optuna_learning_rate.py
@@ -11,6 +11,7 @@ import gc
 
 import optuna
 import numpy as np
+import pandas as pd
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RepeatedKFold
